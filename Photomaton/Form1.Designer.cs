04933b474852d3d8fb09711d9231923cdabfb796
Ronan <ronandoudies@gmail.com>
2023-12-22 09:23:35 +0100

    Initial Commit

---
+++ Photomaton/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Photomaton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.takePictureButton = new System.Windows.Forms.Button();
+            this.previewPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.previewPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // takePictureButton
+            // 
+            this.takePictureButton.AutoSize = true;
+            this.takePictureButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.takePictureButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.takePictureButton.Location = new System.Drawing.Point(0, 882);
+            this.takePictureButton.Name = "takePictureButton";
+            this.takePictureButton.Size = new System.Drawing.Size(1904, 36);
+            this.takePictureButton.TabIndex = 0;
+            this.takePictureButton.Text = "Prendre la photo";
+            this.takePictureButton.UseVisualStyleBackColor = false;
+            // 
+            // previewPictureBox
+            // 
+            this.previewPictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.previewPictureBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.previewPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.previewPictureBox.Name = "previewPictureBox";
+            this.previewPictureBox.Size = new System.Drawing.Size(1880, 852);
+            this.previewPictureBox.TabIndex = 2;
+            this.previewPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 918);
+            this.Controls.Add(this.previewPictureBox);
+            this.Controls.Add(this.takePictureButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Photomaton";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.previewPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button takePictureButton;
+        private System.Windows.Forms.PictureBox previewPictureBox;
+    }
+}
+
---
+++ Photomaton/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Photomaton
+{
+    public partial class Form1 : Form
+    {
+        private VideoCaptureDevice videoSource;
+        private FilterInfoCollection videoDevices;
+        private Bitmap previousFrame;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize webcam
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count > 0)
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+                videoSource.VideoResolution = videoSource.VideoCapabilities[16];
+                videoSource.NewFrame += new NewFrameEventHandler(VideoSource_NewFrame);
+                videoSource.Start();
+            }
+            else
+            {
+                MessageBox.Show("Aucune webcam trouvée!");
+            }
+
+            takePictureButton.Click += TakePictureButton_Click;
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // Update the previewPictureBox with the current webcam frame
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+
+            // Dispose the previous frame
+            previewPictureBox.Image?.Dispose();
+
+            // Update the previewPictureBox
+            previewPictureBox.Image = bitmap;
+        }
+
+        private void TakePictureButton_Click(object sender, EventArgs e)
+        {
+            // Stop the webcam
+            videoSource.SignalToStop();
+            videoSource.WaitForStop();
+
+            // Display a popup asking the user if they want to keep the picture or take a new one
+            DialogResult result = MessageBox.Show("Voulez-vous garder cette photo ?", "Photomaton", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                // Save the picture to the user's "Pictures" folder
+                string picturesFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                string fileName = $"{DateTime.Now:yyyyMMddHHmmss}.png"; // Unique filename based on timestamp
+
+                // Save the picture with a unique filename in the "Pictures" folder
+                string filePath = System.IO.Path.Combine(picturesFolderPath, fileName);
+                previewPictureBox.Image.Save(filePath, System.Drawing.Imaging.ImageFormat.Png);
+
+                MessageBox.Show($"Photo enregistrée avec succès:\n{filePath}");
+            }
+
+            // Restart the webcam for a new session
+            videoSource.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Stop the webcam if it is running
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                // Unsubscribe from the NewFrame event before stopping the webcam
+                videoSource.NewFrame -= new NewFrameEventHandler(VideoSource_NewFrame);
+
+                // Stop the webcam and wait for it to stop
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                videoSource = null;  // Set to null to ensure it is properly disposed
+            }
+
+            // Dispose of the previous frame bitmap
+            previousFrame?.Dispose();
+        }
+    }
+}
